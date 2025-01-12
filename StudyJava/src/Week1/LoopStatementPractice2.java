8e982288dd2e68fe1c753acf31123849afc25f96
2jiyong <jdragon2000@naver.com>
2025-01-12 22:39:44 +0900

    백준 반복문 문제 1,2

---
+++ StudyJava/src/Week1/LoopStatementPractice2.java
@@ -0,0 +1,17 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class LoopStatementPractice2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String nums = sc.next();
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(String.valueOf(nums.charAt(i)));
+        }
+        System.out.println(sum);
+    }
+
+}
