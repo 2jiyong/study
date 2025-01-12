574a34512be611f4566680a024d01b0bdde24f51
2jiyong <jdragon2000@naver.com>
2025-01-12 22:54:40 +0900

    구구단 문제

---
+++ StudyJava/src/Week1/TimesTable.java
@@ -0,0 +1,12 @@
+package Week1;
+
+public class TimesTable {
+    public static void main(String[] args) {
+        int num = 9;
+        for (int i = 2; i<=num; i++){
+            for (int j = 1; j<=num; j++){
+                System.out.printf("%d"+" X "+ "%d = %d\n", i, j, i*j);
+            }
+        }
+    }
+}
